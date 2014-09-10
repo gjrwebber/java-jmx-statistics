fd5c041664b28da2b56616b6bb28b3da96e50907
Graham Webber <gjrwebber@gmail.com>
2014-09-10 17:09:07 +1000

    Added an internal method execution in sample to showcase Aspectj over Spring.

--- samples/java-jmx-statistics-samples-shared/src/main/java/org/gw/samples/service/ExampleServiceImpl.java
+++ samples/java-jmx-statistics-samples-shared/src/main/java/org/gw/samples/service/ExampleServiceImpl.java
@@ -17,18 +17,29 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Creates a new Statisitc called Example5 and associates it with
+     * Creates a new Statisitc called AlwaysIncrement and associates it with
      * ExampleServiceImpl.class. This will register the statistics with the
      * ObjectName: example.service.stats:type=MyAnnotatedService This statistic
-     * will increment on every execution of this method
+     * will increment on every execution of this method. If using AspectJ for
+     * setup PrivateInternalIncrement will equal this statistic.
      */
     @IncrementStat("AlwaysIncrement")
     public void AlwaysIncrement() {
         // .. do something
+        PrivateInternalIncrement();
+    }
+
+
+    /**
+     * Same as AlwaysIncrement, though it will only increment when using AspectJ for setup
+     */
+    @IncrementStat("PrivateInternalIncrement")
+    private void PrivateInternalIncrement() {
+        // .. do something
     }
 
     /**
-     * Same as Example5, though it will only increment if the method returns true
+     * Same as AlwaysIncrement, though it will only increment if the method returns true
      */
     @IncrementStat(value = "IncrementOnReturnTrue", condition = "#result == true")
     public boolean IncrementOnReturnTrue() {
@@ -37,7 +48,7 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Similar to Example5, however for every value of the parameter 'name' will
+     * Similar to AlwaysIncrement, however for every value of the parameter 'name' will
      * result in an individual statistic named 'Dynamic #name' where #name is
      * the value of the parameter at runtime
      */
@@ -48,7 +59,7 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Same as Example6, though it will only increment if the provided name equals
+     * Same as IncrementOnReturnTrue, though it will only increment if the provided name equals
      * 'Harry' and the method returns true
      */
     @IncrementStat(value = "NameEqualsHarry", condition = "#name == 'Harry'")
@@ -58,7 +69,7 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Same as Example7, but it will also log the statistic if logging is enabled
+     * Same as NameEqualsHarry, but it will also log the statistic if logging is enabled
      * for debug
      */
     @IncrementStat(value = "NameEqualsHarry", condition = "#name == 'Harry' && #result == true", logLevel = Statistic.LOG_LEVEL.DEBUG)
@@ -68,7 +79,7 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Same as Example5, though it will only increment if the given
+     * Same as AlwaysIncrement, though it will only increment if the given
      * ServiceException is thrown
      */
     @IncrementStat(value = "IncrementOnServiceException", throwing = ServiceException.class)
@@ -90,7 +101,7 @@ public abstract class ExampleServiceImpl implements ExampleService {
     }
 
     /**
-     * Same as Example5, though it will also provide an average count of the last 1
+     * Same as AlwaysIncrement, though it will also provide an average count of the last 1
      * second window
      */
     @IncrementStat(value = "AverageOverSecond", rollingAvgWindow = AveragingStatistic.ROLLING_AVG_WINDOW.SECOND)
--- samples/java-jmx-statistics-samples-spring/src/main/java/org/gw/samples/service/SpringService.java
+++ samples/java-jmx-statistics-samples-spring/src/main/java/org/gw/samples/service/SpringService.java
@@ -1,8 +1,10 @@
 package org.gw.samples.service;
 
 import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Service;
 
 @ManagedResource(objectName = "org.gw.samples.springservice:name=SpringService")
+@Service
 public class SpringService extends ExampleServiceImpl {
 
 }
