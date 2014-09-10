5e97185acc4ec8e6d5dc5caafb79096255244020
Graham Webber <gjrwebber@gmail.com>
2014-09-10 17:08:32 +1000

    Improved Spring Java config sample

--- samples/java-jmx-statistics-samples-spring/src/main/java/org/gw/samples/AppConfig.java
+++ samples/java-jmx-statistics-samples-spring/src/main/java/org/gw/samples/AppConfig.java
@@ -1,32 +1,19 @@
 package org.gw.samples;
 
 import org.gw.samples.config.SharedConfig;
-import org.gw.samples.service.ExampleService;
-import org.gw.samples.service.SpringService;
-import org.gw.stats.aop.AnnotationDrivenJMXStatisticsService;
 import org.gw.stats.aop.MBeanServerStatisticsSource;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
-import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.*;
 
 @Configurable
+@ComponentScan(basePackages = {"org.gw"})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableLoadTimeWeaving
 @EnableMBeanExport
 public class AppConfig extends SharedConfig {
 
     @Bean
-    public ExampleService exampleService() {
-        return new SpringService();
-    }
-
-    @Bean
-    public AnnotationDrivenJMXStatisticsService statsService() {
-
-        // Instantiate the stats Aspect
-        AnnotationDrivenJMXStatisticsService statsService = new AnnotationDrivenJMXStatisticsService();
+    public MBeanServerStatisticsSource mBeanServerStatisticsSource() {
 
 		/*
          * This will check the mbean server for ObjectNames starting with
@@ -35,9 +22,6 @@ public class AppConfig extends SharedConfig {
 		 */
         MBeanServerStatisticsSource source = new MBeanServerStatisticsSource();
         source.setDomainPrefixFilters("org.gw.samples");
-
-        statsService.setStatsSources(source);
-
-        return statsService;
+        return source;
     }
 }
